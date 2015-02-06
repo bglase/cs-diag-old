bd5896bafa2c2b2aa740b65ec527fa98c1b12c0c
bglase <bglase@icloud.com>
2015-02-06 06:48:57 -0600

    updates

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
  * Entry point for CS Diagnostics
  * 
  * This module handles command line arguments and initiates the requested actions.
+ * It is script friendly; especially with the -q option to supress info messages.
+ * The -v option can be used for more detailed status information
  * 
  * Usage:
  *  node index.js --h for help
@@ -17,6 +19,9 @@ const WEBSERVER_PORT = 3000;
 // Read the default configuration (which may be overridden with command line options)
 var config = require('./config.json');
 
+// Declare the serial port interface
+var portManager = require('cs-controller');
+
 // Retrieve the command line arguments and store them in an object:
 // example: -x 3 -y 4 -n5 -abc --beep=boop foo bar baz
 // yields: { _: [ 'foo', 'bar', 'baz' ], x: 3, y: 4, n: 5, a: true,
@@ -26,49 +31,55 @@ var argv = require( 'minimist' )( process.argv.slice( 2 ) );
 // Check verbose and quiet options
 if( 'v' in argv ) config.verbose = true;
 if( 'q' in argv ) config.quiet = true;
-
+console.log(argv);
 // Check for the help option and if requested, display help
-if( 'h' in argv )
+if( ('h' in argv) || ('help' in argv) )
 {
     console.info( 
         '\nController Diagnostics\n\n',
           
         // Help
-        '-h           : Print help\n',
-        '-list        : List available ports\n',
+        'list         : List available ports\n',
           
         // Firmware management 
-        '-update file : Update firmware using specified file\n',
+        'update file  : Update firmware using specified file\n',
 
         // EEPROM configuration management
-        '-config file : Update EEPROM configuration using specified file\n',
+        'config file  : Update EEPROM configuration using specified file\n',
           
         // Application control
-        '-p port      : Name of serial port to use\n',
-        '-q           : quiet output (helpful for scripting)\n',
-        '-v           : verbose output (helpful for debugging)\n',
-        '-w           : start web server\n',
-        '-wc          : start web server and launch browser client\n',
+        '-p port1[,port2...]    : List of serial port to use\n',
+        '-q                     : Quiet output (helpful for scripting)\n',
+        '-v                     : Verbose output (helpful for debugging)\n',
+        '-w                     : Start web server\n',
+        '-wc                    : Start web server and launch browser client\n',
+        '-h                     : Print help and exit\n',
         '\n'
          );
     
     process.exit( EXIT_SUCCESS );
 }
-else if( 'list' in argv )
+else if( 0 === argv._.indexOf( 'list') )
 {
     // list ports option: display them
-    console.info( '\nAvailable Serial Ports:\n' );
-/*    serialPort.list( function( err, ports )
+    portManager.list( function( err, ports )
         {
-            ports.forEach( function(port)
-            {
-                console.info( port.comName, '\t',
-                    port.pnpId, '\t',
-                    port.manufacturer
-                );
-            } );
+            if( err ) {
+                console.error('err ' + err);
+                return EXIT_FAILURE;
+            }
+            else {
+                console.info( '\nAvailable Serial Ports:' );
+                ports.forEach( function(port)
+                {
+                    console.info( port.comName, '\t',
+                        port.pnpId, '\t',
+                        port.manufacturer
+                    );
+                } );
+                return EXIT_SUCCESS;
+            };
         } );
-*/
 }
 else
 {
@@ -114,7 +125,7 @@ else
                       console.log('err ' + err);
                       console.log('results ' + results);
                   });
-              }
+              };
           });
     }
     else
